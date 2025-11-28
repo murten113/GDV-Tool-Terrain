e4f25207e1105468bc51d74539d85dbc728daabd
murmur76 <67921214+murten113@users.noreply.github.com>
2025-11-28 21:27:07 +0100

    Started main UI logic

--- Assets/scripts/UI/Collapsible Panels/CollapsiblePanel.cs
+++ Assets/scripts/UI/Collapsible Panels/CollapsiblePanel.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
 
+// This script is used to control the collapsible panel in the UI.
+
 public class CollapsiblePanel : MonoBehaviour
 {
+
+    [Header("Panel Settings")]
+    [SerializeField] private RectTransform panelRectTransform;
+    [SerializeField] private float animationSpeed = 500f;
+
+    [Header("States")]
+    private bool isExpanded = true;
+    private Vector2 expandedPosition;
+    private Vector2 collapsedPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
