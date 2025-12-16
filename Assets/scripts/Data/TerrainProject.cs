9d7caf6083cd900fd72a95c588ce77066d3d998f
murmur76 <67921214+murten113@users.noreply.github.com>
2025-12-16 15:58:36 +0100

    Update TerrainProject.cs

--- Assets/scripts/Data/TerrainProject.cs
+++ Assets/scripts/Data/TerrainProject.cs
@@ -1,7 +1,39 @@
 using System;
 using UnityEngine;
 
+
+[Serializable]
 public class TerrainProject : MonoBehaviour
 {
-    
+    [Header("Project Metadata")]
+    public string projectName = "New Project";
+    public string version = "1.0";
+    public string creationDate;
+    public string lastModifiedDate;
+
+    [Header("Terrain Data")]
+    public TerrainData terrainData;
+
+    public TerrainProject()
+    {
+        projectName = "New Project";
+        version = "1.0";
+        creationDate = System.DateTime.Now.ToString();
+        lastModifiedDate = creationDate;
+        terrainData = new TerrainData();
+    }
+
+    public TerrainProject(string name, TerrainData data)
+    {
+        projectName = name;
+        version = "1.0";
+        creationDate = System.DateTime.Now.ToString();
+        lastModifiedDate = creationDate;
+        terrainData = data;
+    }
+
+    public void UpdateLastModified()
+    {
+        lastModifiedDate = System.DateTime.Now.ToString();
+    }
 }
